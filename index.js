f4819513f24eec65f6aa78b777017c7f46536f92
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2020-02-29 14:35:54 +0900

    Update get and post function

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 'use strict';
 const http = require('http');
+const now = new Date();
 const server = http.createServer((req, res) => {
-  console.info('[' + new Date() + '] Requested by ' + req.connection.remoteAddress);
+  console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
   res.writeHead(200, {
     'Content-Type': 'text/plain; charset=utf-8'
   });
   res.write(req.headers['user-agent']);
+
+  switch(req.method) {
+    case 'GET':
+      res.write('GET ' + req.url);
+      break;
+    case 'POST':
+      res.write('POST ' + req.url);
+      let rawData = '';
+      req.on('data', (chunk) => {
+        rawData = rawData + chunk;
+      }).on('end', () => {
+        console.info('[' + now + '] Data posted: ' + rawData);
+      })
+      break;
+    default:
+      break;
+  }
+
   res.end();
 }).on('error', (e) => {
-  console.error('[' + new Date() + '] Server Error', e);
+  console.error('[' + now + '] Server Error', e);
 }).on('clientError', (e) => {
-  console.error('[' + new Date() + '] Client Error', e);
+  console.error('[' + now + '] Client Error', e);
 });
 const port = 8000;
 server.listen(port, () => {
-  console.info('[' + new Date() + '] Listening on ' + port);
+  console.info('[' + now + '] Listening on ' + port);
 });
